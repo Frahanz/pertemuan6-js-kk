be4650bcc7d0c4908d15f258754b17499e719b3d
Frahanz <farhanrahmat321@gmail.com>
2023-11-18 11:38:02 +0700

    first commit

---
+++ ifelse.js
@@ -0,0 +1,29 @@
+// let kelamin = 'cowok'
+// if(kelamin==='cowok'){
+//     console.log('anda adalah cowok')
+// }else if(kelamin==='cewek'){
+//     console.log('anda adalah cewek')
+// }else{
+//     console.log('Mohon maaf gender yang anda masukkan salah')
+// }
+// console.log('terima kasih')
+
+
+// var nama = prompt('masukkan nama anda')
+// if(nama === 'farhan'){
+//     alert(nama= 'Anda adalah cowok')
+// }else{
+//     alert(nama= 'Anda adalah cewek')
+// }
+
+const nilai = prompt('Masukkan Nilai Anda')
+if(nilai>90){
+    alert('Grade A')
+}else if(nilai>80){
+    alert('Grade B+')
+}else if(nilai>70){
+    alert('Grade B')
+}else{
+    alert('Grade F')
+}
+alert('Tingkatkan lagi belajarnya')
